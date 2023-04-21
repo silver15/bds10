c84a410ab4a8d6fd3b3e5986e41a96056e1ef642
Luiz Carlos Silvestrini <luizsilvestrini15@hotmail.com>
2023-04-21 20:02:39 -0300

    Atualização List e Form

--- frontweb/src/pages/Admin/Employees/Form/index.tsx
+++ frontweb/src/pages/Admin/Employees/Form/index.tsx
@@ -1,37 +1,154 @@
+import { useHistory, useParams } from 'react-router-dom';
 import './styles.css';
+import { Employee } from 'types/employee';
+import { Controller, useForm } from 'react-hook-form';
+import Select from 'react-select';
+import { useEffect, useState } from 'react';
+import { Department } from 'types/department';
+import { requestBackend } from 'util/requests';
+import { AxiosRequestConfig } from 'axios';
+import { toast } from 'react-toastify';
+
+type UrlParams = {
+  employeeId: string;
+};
 
 const Form = () => {
 
-  const handleCancel = () => {
-    // to do
+  const { employeeId } = useParams<UrlParams>();
+
+  const [selectDepartments, setSelectDepartments] = useState<Department[]>([]);
+
+  const isEditing = employeeId !== 'create';
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setValue,
+    control,
+  } = useForm<Employee>();
+
+
+  useEffect(() => {
+    requestBackend({ url: '/departments', withCredentials:true  }).then((response) => {
+      setSelectDepartments(response.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (isEditing) {
+      requestBackend({ url: `/employees/${employeeId}` }).then((response) => {
+        const employee = response.data as Employee;
+
+
+        setValue('name', employee.name);
+        setValue('email', employee.email);
+        setValue('department', employee.department);
+      });
+    }
+  }, [isEditing, employeeId, setValue]);
+
+  const onSubmit = (formData: Employee) => {
+    const data = {
+      ...formData,
+
+    };
+
+    const config: AxiosRequestConfig = {
+      method: isEditing ? 'PUT' : 'POST',
+      url: isEditing ? `/employees/${employeeId}` : '/employees',
+      data,
+      withCredentials: true,
+    };
+
+    requestBackend(config)
+      .then(() => {
+        toast.info('Cadastrado com sucesso')
+        history.push('/admin/employees');
+
+      })
+      .catch(() => {
+        toast.error('Erro ao cadastrar produto')
+      })
   };
+
+
+  const history = useHistory();
+
+  const handleCancel = () => {
+    history.push('/admin/employees');
+  };
+
 
   return (
     <div className="employee-crud-container">
       <div className="base-card employee-crud-form-card">
         <h1 className="employee-crud-form-title">INFORME OS DADOS</h1>
 
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="row employee-crud-inputs-container">
             <div className="col employee-crud-inputs-left-container">
 
               <div className="margin-bottom-30">
-                <input type="text" 
-                  className="form-control base-input is-invalid"
+                <input
+                  {...register('name', {
+                    required: 'Campo obrigatório',
+                  })}
+                  type="text"
+                  className={`form-control base-input ${errors.name ? 'is-invalid' : ''
+                    }`}
+                  placeholder="Nome do Funcionario"
+                  name="name"
+                  data-testid="name"
                 />
                 <div className="invalid-feedback d-block">
-                  Mensagem de erro
+                  {errors.name?.message}
                 </div>
               </div>
 
               <div className="margin-bottom-30">
-                <input type="text" 
-                  className="form-control base-input"
+                <input
+                  {...register('email', {
+                    required: 'Campo obrigatório',
+                    pattern: {
+                      value: /\S+@\S+\.\S+/,
+                      message: 'Email inválido',
+                    },
+                  })}
+                  type="text"
+                  className={`form-control base-input ${errors.name ? 'is-invalid' : ''
+                    }`}
+                  placeholder="Email do funcionario"
+                  name="email"
+                  data-testid="email"
                 />
                 <div className="invalid-feedback d-block">
-                  
+                  {errors.email?.message}
                 </div>
               </div>
+              <label htmlFor="department" className="d-none">Departamento</label>
+              <Controller
+                name="department"
+                rules={{ required: true }}
+                control={control}
+                render={({ field }) => (
+                  <Select
+                    {...field}
+                    options={selectDepartments}
+                    classNamePrefix="product-crud-select"
+                    getOptionLabel={(department: Department) => department.name}
+                    getOptionValue={(department: Department) => String(department.id)
+                    }
+                    inputId="department"
+                  />
+                )}
+              />
+              {errors.department && (
+                <div className="invalid-feedback d-block">
+                  Campo obrigatório
+                </div>
+              )}
 
             </div>
           </div>
--- frontweb/src/pages/Admin/Employees/List/index.tsx
+++ frontweb/src/pages/Admin/Employees/List/index.tsx
@@ -1,24 +1,58 @@
-import './styles.css';
-
 import Pagination from 'components/Pagination';
 import EmployeeCard from 'components/EmployeeCard';
 import { Link } from 'react-router-dom';
+import { useCallback, useEffect, useState } from 'react';
+import { SpringPage } from 'types/vendor/spring';
+import { Employee } from 'types/employee';
+import { AxiosRequestConfig } from 'axios';
+import { requestBackend } from 'util/requests';
+import { getEmployessResponse } from '../__tests__/fixtures';
 
-const employeeHardCode = { // delete
-  id: 1,
-  name: "Carlos",
-  email: "carlos@gmail.com",
-  department: {
-    id: 1,
-    name: "Sales"
-  }
-};
+import './styles.css';
+
+type ControlComponentsData = {
+  activePage: number;
+}
+
 
 const List = () => {
 
-  const handlePageChange = (pageNumber: number) => {
-    // to do
+  const [page, setPage] = useState<SpringPage<Employee>>();
+
+  const [controlComponentsData, setControlComponentsData] = useState<ControlComponentsData>(
+    {
+      activePage: 0
+    }
+  );
+
+
+  const handlePagechange = (pageNumber: number) => {
+    setControlComponentsData({ activePage: pageNumber });
   };
+
+
+
+  const getEmployees = useCallback(() => {
+    const config: AxiosRequestConfig = {
+      method: 'GET',
+      url: '/employees',
+      params: {
+        page: controlComponentsData.activePage,
+        size: 12,
+      },
+    };
+
+
+    requestBackend(config)
+      .then((response) => {
+        setPage(response.data);
+      });
+  }, [controlComponentsData]);
+
+  useEffect(() => {
+    getEmployees();
+  }, [getEmployees]);
+
 
   return (
     <>
@@ -28,16 +62,18 @@ const List = () => {
         </button>
       </Link>
 
-      <EmployeeCard employee={employeeHardCode} />
-      <EmployeeCard employee={employeeHardCode} />
-      <EmployeeCard employee={employeeHardCode} />
-      <EmployeeCard employee={employeeHardCode} />
+      {page?.content.map((employee) => (
+        <div key={employee.id}>
+          <EmployeeCard employee={employee} />
+        </div>
+      ))}
+
 
       <Pagination
         forcePage={0}
         pageCount={1}
         range={3}
-        onChange={handlePageChange}
+        onChange={handlePagechange}
       />
     </>
   );
